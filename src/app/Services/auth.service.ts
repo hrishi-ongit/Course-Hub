c1dab9e59156faae2266811d08cdd26a2a5dbe9b
Hrishikesh Kannikar <hrishikeshkannikar@gmail.com>
2025-04-08 02:47:36 +0530

    Added CanActivate auth guard in routing

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,28 +1,48 @@
 import { Injectable, inject } from "@angular/core";
 import { UserService } from "./user.service";
+import { User } from "../Models/user";
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthService{
-    isLogged: Boolean = false;
+export class AuthService {
+    isLoggedIn: boolean = false;
     userService: UserService = inject(UserService);
 
-    login(username: string, password: string){
-        let user = this.userService.users.find((u) => u.username === username 
-                                                    && u.password === password);
-        if(user === undefined)
-            this.isLogged = false;
-        else
-            this.isLogged = true;
+
+    login(userName: string, password: string): User {
+        const user = this.userService.users.find(user => user.username === userName && user.password === password);
+        this.isLoggedIn = user ? true : false;
         return user;
     }
-
-    logout(){
-        this.isLogged = false;
+    logout(): void {
+        this.isLoggedIn = false;
     }
-
-    IsAuthenticated(){
-        return this.isLogged;
+    authenticated() {
+        return this.isLoggedIn;
     }
 }
+
+
+// export class AuthService{
+//     isLogged: Boolean = false;
+//     userService: UserService = inject(UserService);
+
+//     login(username: string, password: string){
+//         let user = this.userService.users.find((u) => u.username === username 
+//                                                     && u.password === password);
+//         if(user === undefined)
+//             this.isLogged = false;
+//         else
+//             this.isLogged = true;
+//         return user;
+//     }
+
+//     logout(){
+//         this.isLogged = false;
+//     }
+
+//     IsAuthenticated(){
+//         return this.isLogged;
+//     }
+// }
--- src/app/Services/authguard.service.ts
+++ src/app/Services/authguard.service.ts
@@ -6,42 +6,55 @@ import { ContactComponent } from '../contact/contact.component';
 import { Course } from '../Models/course';
 import { CourseService } from './course.service';
 
-export interface IDeactivateComponent{
-    canExit: () => boolean | Observable<boolean> | Promise<boolean>;
-}
+// export interface IDeactivateComponent{
+//     canExit: () => boolean | Observable<boolean> | Promise<boolean>;
+// }
 
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate, CanActivateChild, CanDeactivate<IDeactivateComponent>, Resolve<Course[]>{
+export class AuthGuardService implements CanActivate {
     authService: AuthService = inject(AuthService);
     router: Router = inject(Router);
-    courseService: CourseService = inject(CourseService);
-
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
-        boolean | Observable<boolean> | Promise<boolean>
-    {
-        if(this.authService.IsAuthenticated()){
-            return true;
-        }else{
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean>{
+        // return false;
+        const isLoggedIn = this.authService.authenticated();
+        if(isLoggedIn) return true;// this will activate the child route
+        else{
             this.router.navigate(['/Login']);
             return false;
         }
     }
-
-
-    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.canActivate(childRoute, state);
-    }
-
-    canDeactivate(component: IDeactivateComponent, currentRoute: ActivatedRouteSnapshot, 
-        currentState: RouterStateSnapshot, nextState: RouterStateSnapshot) 
-    {
-        return component.canExit();
-    }
-
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Course[] | Observable<Course[]> | Promise<Course[]> {
-        return this.courseService.getAllcourses();
-    }
 }
+// export class AuthGuardService implements CanActivate, CanActivateChild, CanDeactivate<IDeactivateComponent>, Resolve<Course[]>{
+//     authService: AuthService = inject(AuthService);
+//     router: Router = inject(Router);
+//     courseService: CourseService = inject(CourseService);
+
+//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+//         boolean | Observable<boolean> | Promise<boolean>
+//     {
+//         if(this.authService.IsAuthenticated()){
+//             return true;
+//         }else{
+//             this.router.navigate(['/Login']);
+//             return false;
+//         }
+//     }
+
+
+//     canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+//         return this.canActivate(childRoute, state);
+//     }
+
+//     canDeactivate(component: IDeactivateComponent, currentRoute: ActivatedRouteSnapshot, 
+//         currentState: RouterStateSnapshot, nextState: RouterStateSnapshot) 
+//     {
+//         return component.canExit();
+//     }
+
+//     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Course[] | Observable<Course[]> | Promise<Course[]> {
+//         return this.courseService.getAllcourses();
+//     }
+// }
