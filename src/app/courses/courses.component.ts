39562463b01e14d1cdda38094916d71f1e934422
Hrishikesh Kannikar <hrishikeshkannikar@gmail.com>
2025-04-12 01:04:59 +0530

    Added new shared service and simplified async code in it, added dynamic code to fetch and update data

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,6 +3,9 @@ import { Course } from '../Models/course';
 import { CourseService } from '../Services/course.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Constants } from '../constants/data.constants';
+import { from, map, Observable } from 'rxjs';
+import { SharedService } from '../shared/shared.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
@@ -15,39 +18,48 @@ export class CoursesComponent implements OnInit {
   constants: Constants = inject(Constants);
   activeRoute: ActivatedRoute = inject(ActivatedRoute);
   router: Router = inject(Router);
+  sharedService = inject(SharedService);
 
   public description:string = this.constants.courseDesc;
-  // public description:string = '';
-  // AllCourses: Course[];
-  AllCourses: Course[] = this.coursesService.courses;
-  displayCourses: Course[];
+  AllCourses: Course[];//for internal filtering after one time fetching
+  displayCourses: Course[];// for dynamically updating and rendering on each fetch
   searchString: string;
   searchQueryString: string;
 
 
   ngOnInit(){
-    this.searchQueryString = this.activeRoute.snapshot.queryParams['search'];
+    //using activatedRoute snapshot ..(only latest/most recent values in received) - 
+    // this.searchQueryString = this.activeRoute.snapshot.queryParams['search'];
     // this.searchQueryString = this.activeRoute.snapshot.queryParamMap.get('search');
-    // console.log('Route query from snapshot ngOnInit : ',this.searchQueryString);
-    if(this.searchQueryString === undefined || this.searchQueryString === '' || this.searchQueryString === null){
-      this.displayCourses = JSON.parse(JSON.stringify(this.coursesService.courses));
-    }
-    else {
-      this.populateCoursesFromSearch(this.searchQueryString);
-    }
 
+    //Using queryParams/queryParamMap - 
     //internal component route with query param 
     //old/depricated
     // this.activeRoute.queryParams.subscribe((value) => {
     //   this.searchQueryString = value['search'];
     //   this.populateCoursesFromSearch(this.searchQueryString);
     // })
-    //new :- 
-    // this.activeRoute.queryParamMap.subscribe((value) => {
-    //   this.searchQueryString = value.get('search');
-    //   this.populateCoursesFromSearch(this.searchQueryString);
-    // })
     
+    //new :- 
+    this.activeRoute.queryParamMap.subscribe((value) => {
+      this.searchQueryString = value.get('search');
+    })
+
+    //with snapshot/queryParams/queryParamMap, get the search param in routr 
+    // (if used query in url..defined or just navigation ..undefined, based on such below code)
+    //When no query (direct navigation/course page load, load all courses displayCourses)
+    if(this.searchQueryString === undefined || this.searchQueryString === '' || this.searchQueryString === null){
+       this.sharedService.getAllCourses().subscribe((res) => {
+        this.AllCourses = res;
+        this.displayCourses = JSON.parse(JSON.stringify(this.AllCourses));
+      })
+    }
+    //for query search in route url..
+    else {
+      this.populateCoursesFromSearch(this.searchQueryString);
+    }
+
+
     // this.displayCourses = JSON.parse(JSON.stringify(this.AllCourses));
     // this.searchString = this.activeRoute.snapshot.queryParams['search'];
     // this.searchString = this.activeRoute.snapshot.queryParamMap.get('search');
@@ -69,14 +81,23 @@ export class CoursesComponent implements OnInit {
     //   }
     // })
 
-    
   }
 
-  //search result from route query params 
+  //filter search result from source based on route query params value 
   populateCoursesFromSearch(searchQuery: string) {
-    const allCourses = JSON.parse(JSON.stringify(this.coursesService.courses));
-    this.displayCourses = allCourses.filter(courses => courses.title.toLowerCase().includes(searchQuery.toLowerCase())) || [];
+    this.sharedService.searchCourses(searchQuery).subscribe((filteredCourses) => {
+      this.displayCourses = filteredCourses;  
+    });
   }
+  //this is moved to shared service
+  // getAllCourses(searchQuery: string): Observable<Course[]> {
+  //   return this.coursesService.getAllcourses().pipe(
+  //     map((courses:Course[]) => courses.filter(course => course.title.toLowerCase().includes(searchQuery.toLowerCase())))
+  //   );
+  // }
+
+
+
 
   //When redirected from home component search bander, we receive search query here -
   // ngOnChanges(): void {
@@ -85,12 +106,17 @@ export class CoursesComponent implements OnInit {
   //   console.log('Route query from ',searchedCourse);
   // }
 
-  //For internal filtering the courses
+  //For internal filtering the courses statically on received data
   OnSearchClicked(searchString: string) {
-    if(!searchString || searchString == '') this.displayCourses = JSON.parse(JSON.stringify(this.AllCourses));
-    let allCourses = JSON.parse(JSON.stringify(this.AllCourses));
-    this.displayCourses = allCourses.filter(course => course.title.toLowerCase().includes(searchString.toLowerCase()));
+    if(!searchString || searchString == ''){
+      this.displayCourses = JSON.parse(JSON.stringify(this.AllCourses))
+    } 
+    else {
+      let allCourses = JSON.parse(JSON.stringify(this.AllCourses));
+      this.displayCourses = allCourses.filter(course => course.title.toLowerCase().includes(searchString.toLowerCase()));
+    }
   }
+
 
 
   // Using internal route with query params, using this. 
---
+++ src/app/shared/shared.service.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Course } from '../Models/course';
+import { CourseService } from '../Services/course.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  private courseService = inject(CourseService);
+  constructor() { }
+
+  public getAllCourses(): Observable<Course[]> {
+    return this.courseService.getAllcourses();
+  }
+
+  public searchCourses(searchString: string): Observable<Course[]>{
+    return this.courseService.getAllcourses().pipe(
+      // map((courses: Course[]) => {
+      //   return courses.filter(course => course.title.toLowerCase().includes(queryString));
+      // }) or..
+      map((courses: Course[]) => courses.filter(course => course.title.toLowerCase().includes(searchString.toLowerCase())))
+    );
+  }
+}
